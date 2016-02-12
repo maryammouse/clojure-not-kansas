036e3e5ce8b8c794862fffbd5cb194dda98c95c8
Maryaeria <maryamsyed2096@gmail.com>
2016-02-12 09:07:45 -0800

    Readd training-day through iloveponies directory, no longer submodule

---
+++ iloveponies/training-day/src/training_day.clj
@@ -0,0 +1,31 @@
+(ns training-day)
+
+(def answer "42")
+
+(def hai "O HAI!")
+
+(defn square [x]
+  ":(")
+
+(defn average [a b]
+  ":(")
+
+(defn
+  hello
+  "Gives out personalized greetings."
+  [who]
+  (str "Hello, " who "!"))
+
+(defn
+  square
+  "Multiplies a number by itself."
+[x]
+(* x x)
+  )
+
+(defn
+  average
+  "Returns the average of two numbers."
+  [x y]
+  (/ (+ x y) 2))
+
