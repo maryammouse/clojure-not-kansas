77e6eb0df8f4a9a50614bfb006560fcb654bf73c
Maryaeria <maryamsyed2096@gmail.com>
2016-02-24 11:42:49 -0800

    Start learning the secrets of structured data in Clojure

---
+++ iloveponies/structured-data/src/structured_data.clj
@@ -0,0 +1,117 @@
+(ns structured-data)
+
+(defn do-a-thing [x]
+  (let [doubled-x (+ x x)])
+  (Math/pow (+ x x) (+ x x)))
+
+(defn spiff [v]
+  (+ (get v 0) (get v 2)))
+
+(defn cutify [v]
+  (conj v "<3"))
+
+(defn spiff-destructuring [v]
+  (let [[x y z] v]
+    (+ x z)))
+
+
+(defn point [x y]
+  [x y])
+
+(defn rectangle [bottom-left top-right]
+  [bottom-left top-right])
+
+(defn width [rectangle]
+  :-)
+
+(defn height [rectangle]
+  :-)
+
+(defn square? [rectangle]
+  :-)
+
+(defn area [rectangle]
+  :-)
+
+(defn contains-point? [rectangle point]
+  :-)
+
+(defn contains-rectangle? [outer inner]
+  :-)
+
+(defn title-length [book]
+  :-)
+
+(defn author-count [book]
+  :-)
+
+(defn multiple-authors? [book]
+  :-)
+
+(defn add-author [book new-author]
+  :-)
+
+(defn alive? [author]
+  :-)
+
+(defn element-lengths [collection]
+  :-)
+
+(defn second-elements [collection]
+  :-)
+
+(defn titles [books]
+  :-)
+
+(defn monotonic? [a-seq]
+  :-)
+
+(defn stars [n]
+  :-)
+
+(defn toggle [a-set elem]
+  :-)
+
+(defn contains-duplicates? [a-seq]
+  :-)
+
+(defn old-book->new-book [book]
+  :-)
+
+(defn has-author? [book author]
+  :-)
+
+(defn authors [books]
+  :-)
+
+(defn all-author-names [books]
+  :-)
+
+(defn author->string [author]
+  :-)
+
+(defn authors->string [authors]
+  :-)
+
+(defn book->string [book]
+  :-)
+
+(defn books->string [books]
+  :-)
+
+(defn books-by-author [author books]
+  :-)
+
+(defn author-by-name [name authors]
+  :-)
+
+(defn living-authors [authors]
+  :-)
+
+(defn has-a-living-author? [book]
+  :-)
+
+(defn books-by-living-authors [books]
+  :-)
+
+; %________%
