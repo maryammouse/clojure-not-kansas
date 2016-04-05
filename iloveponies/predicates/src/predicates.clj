80063e0278126b50dee23637773d4e875b2b2b73
Maryaeria <maryamsyed2096@gmail.com>
2016-04-05 10:56:52 -0700

    Complete up to and including Ex. 3 of Predicates

--- iloveponies/predicates/src/predicates.clj
+++ iloveponies/predicates/src/predicates.clj
@@ -1,16 +1,27 @@
 (ns predicates)
 
 (defn sum-f [f g x]
-  :-)
+  (+ (f x) (g x)))
+
+(sum-f inc dec 4)
+(sum-f inc identity 5)
+(sum-f identity - 10)
 
 (defn less-than [n]
-  :-)
+  (fn [k] (< k n)))
 
 (defn equal-to [n]
-  :-)
+  (fn [k] (== k n)))
+
+(filter (less-than 3) [1 2 3 4 5])
+(filter (less-than 4) [-2 12 3 4 0])
+(filter (equal-to 2) [2 1 3 2.0])
+(filter (equal-to 2) [3 4 5 6])
 
 (defn set->predicate [a-set]
-  :-)
+  (fn [x] (contains? a-set x)))
+
+(filter (set->predicate #{1 2 3 nil}) [2 nil 4 nil 6])
 
 (defn pred-and [pred1 pred2]
   :-)
