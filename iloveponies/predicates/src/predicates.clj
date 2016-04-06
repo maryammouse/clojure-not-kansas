8f8f0289129458086d1d81ce24c036f93b730c17
Maryaeria <maryamsyed2096@gmail.com>
2016-04-06 10:01:17 -0700

    Complete up to Ex 7 in the Predicates chapter

--- iloveponies/predicates/src/predicates.clj
+++ iloveponies/predicates/src/predicates.clj
@@ -24,16 +24,49 @@
 (filter (set->predicate #{1 2 3 nil}) [2 nil 4 nil 6])
 
 (defn pred-and [pred1 pred2]
-  :-)
+  (fn [x] (and (pred1 x) (pred2 x))))
+
+(filter (pred-and pos? even?) [1 2 -4 0 6 7 -3])
+(filter (pred-and pos? odd?) [1 2 -4 0 6 7 -3])
+(filter (pred-and (complement nil?) empty?) [[] '() nil {} #{}])
 
 (defn pred-or [pred1 pred2]
-  :-)
+  (fn [x] (or (pred1 x) (pred2 x))))
+(filter (pred-or pos? odd?) [1 2 -4 0 6 7 -3])
+(filter (pred-or pos? even?) [1 2 -4 0 6 7 -3])
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn blank? [string]
-  :-)
+  (every? whitespace? string))
+
+(blank? " \t\n\t")
+(blank? " \t a")
+(blank? "")
+(def china {:name "China Miéville", :birth-year 1972})
+(def octavia {:name "Octavia E. Butler"
+              :birth-year 1947
+              :death-year 2006})
+(def kay {:name "Guy Gavriel Kay" :birth-year 1954})
+(def dick {:name "Philip K. Dick", :birth-year 1928, :death-year 1982})
+(def zelazny {:name "Roger Zelazny", :birth-year 1937, :death-year 1995})
+
+(def authors #{china, octavia, kay, dick, zelazny})
+
+(def cities {:title "The City and the City" :authors #{china}
+             :awards #{:locus, :world-fantasy, :hugo}})
+(def wild-seed {:title "Wild Seed", :authors #{octavia}})
+(def lord-of-light {:title "Lord of Light", :authors #{zelazny}
+                    :awards #{:hugo}})
+(def deus-irae {:title "Deus Irae", :authors #{dick, zelazny}})
+(def ysabel {:title "Ysabel", :authors #{kay}, :awards #{:world-fantasy}})
+(def scanner-darkly {:title "A Scanner Darkly" :authors #{dick}})
+
+(def books #{cities, wild-seed, lord-of-light,
+             deus-irae, ysabel, scanner-darkly})
+
+
 
 (defn has-award? [book award]
   :-)
